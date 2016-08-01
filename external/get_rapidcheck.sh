577679c2f612a44a5c25233604ae22225b6ad3ee
Michael F. Herbst <info@michael-herbst.com>
2016-08-01 19:07:05 +0200

    Add missing copyright headers

--- external/get_rapidcheck.sh
+++ external/get_rapidcheck.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2016 by the krims authors
+##
+## This file is part of krims.
+##
+## krims is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## krims is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with krims. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+
 if ! pwd | grep -q 'krims/external$'; then
 	echo "Need to be in krims/external directory."
 	exit 1
