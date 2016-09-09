fa581b9965d56042100d1d66574230d9e403147b
Michael F. Herbst <info@michael-herbst.com>
2016-09-09 21:21:51 +0200

    Reorganised NumComp code
    Various smaller BugFixes and improvements:
    - Both sides of the == may now take different types
    - Constants are a header and a source file
    - NumCompChangeSafe class to safely change the global NumCompConstants
      (such that they are changed back appropriately once this class goes
      out of scope)

---
+++ src/krims/NumComp/NumComp.hh
@@ -0,0 +1,131 @@
+//
+// Copyright (C) 2016 by the krims authors
+//
+// This file is part of krims.
+//
+// krims is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// krims is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with krims. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+#include "NumCompConstants.hh"
+#include "NumEqual.hh"
+#include <limits>
+#include <type_traits>
+
+namespace krims {
+
+/** Struct to provide functions which check allow for numeric-aware comparison
+ * between objects */
+template <typename T>
+class NumComp {
+public:
+  // TODO hard-coded double type
+  typedef typename std::conditional<std::is_floating_point<T>::value, T,
+                                    double>::type error_type;
+
+  /** Construct an NumComp object */
+  explicit NumComp(const T& value)
+        : m_tolerance(0),
+          m_failure_action(NumCompConstants::default_failure_action),
+          m_value(value) {
+    tolerance(NumCompAccuracyLevel::Default);
+  }
+
+  /** Modify the tolerance by providing a different accuracy level */
+  NumComp& tolerance(const NumCompAccuracyLevel accuracy);
+
+  /** Modify the comparison tolerance */
+  NumComp& tolerance(const error_type tolerance) {
+    m_tolerance = tolerance;
+    return *this;
+  }
+
+  NumComp& failure_action(const NumCompActionType failure_action) {
+    m_failure_action = failure_action;
+    return *this;
+  }
+
+  //@{
+  /** Compare with another object for equality */
+  template <typename U>
+  friend bool operator==(const U& lhs, const NumComp& rhs) {
+    return NumEqual<U, T>{rhs.m_tolerance, rhs.m_failure_action}(lhs,
+                                                                 rhs.m_value);
+  }
+
+  template <typename U>
+  friend bool operator==(const NumComp& lhs, const U& rhs) {
+    return rhs == lhs;
+  }
+  //@}
+
+private:
+  /** \brief tolerance when comparing objects */
+  error_type m_tolerance;
+
+  //! The action to perform if a comparison fails
+  NumCompActionType m_failure_action;
+
+  /** The value  */
+  const T& m_value;
+};
+
+/** Helper function to make an NumComp<T> object */
+template <typename T>
+NumComp<T> numcomp(const T& value) {
+  return NumComp<T>(value);
+}
+
+//
+// ----------------------------------------------------------------------------
+//
+
+template <typename T>
+NumComp<T>& NumComp<T>::tolerance(const NumCompAccuracyLevel accuracy) {
+  error_type factor = NumCompConstants::default_tolerance_factor;
+
+  switch (accuracy) {
+    case NumCompAccuracyLevel::MachinePrecision:
+      factor = 1.;
+      break;
+    case NumCompAccuracyLevel::TenMachinePrecision:
+      factor = 10.;
+      break;
+    case NumCompAccuracyLevel::Extreme:
+      factor /= 100.;
+      break;
+    case NumCompAccuracyLevel::Higher:
+      factor /= 10.;
+      break;
+    case NumCompAccuracyLevel::Default:
+      break;
+    case NumCompAccuracyLevel::Lower:
+      factor *= 10.;
+      break;
+    case NumCompAccuracyLevel::Sloppy:
+      factor *= 100.;
+      break;
+    case NumCompAccuracyLevel::SuperSloppy:
+      factor *= 1000.;
+      break;
+  }
+
+  // Make sure factor is at least one:
+  if (factor < 1.) factor = 1.;
+
+  m_tolerance = factor * std::numeric_limits<error_type>::epsilon();
+  return *this;
+}
+
+}  // namespace krims
---
+++ src/krims/NumComp/NumCompException.hh
@@ -0,0 +1,107 @@
+//
+// Copyright (C) 2016 by the krims authors
+//
+// This file is part of krims.
+//
+// krims is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// krims is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with krims. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+#include "krims/ExceptionSystem.hh"
+#include <iomanip>
+
+namespace krims {
+
+/** Exception raised by the NumComp operations if they fail on some objects. */
+template <typename T>
+class NumCompException : public ExceptionBase {
+public:
+  static_assert(std::is_arithmetic<T>::value,
+                "T needs to be an arithmetic value");
+  // otherwise the declaration of T as error and tolerance makes no sense.
+
+  NumCompException(const T lhs_, const T rhs_, const T error_,
+                   const T tolerance_, const std::string operation_string_,
+                   std::string description_ = "") noexcept
+        : lhs(lhs_),
+          rhs(rhs_),
+          error(error_),
+          tolerance(tolerance_),
+          operation_string(operation_string_),
+          description(description_) {}
+
+  //! The value of the lhs
+  const T lhs;
+
+  //! The value of the rhs
+  const T rhs;
+
+  //! The error that was obtained
+  const T error;
+
+  //! The tolerance we applied
+  const T tolerance;
+
+  //! A string describing the operation (like "==" or "!=")
+  const std::string operation_string;
+
+  //! The description that was additionally supplied
+  std::string description;
+
+  //! Append some extra data to the description:
+  void append(const std::string& extra);
+
+  /** Add enhancing exception data */
+  void add_exc_data(const char* file, int line, const char* function);
+
+  /** Print exception-specific extra information to the outstream */
+  virtual void print_extra(std::ostream& out) const noexcept;
+
+private:
+  std::string failed_condition{""};
+};
+
+//
+// --------------------------------------------------------------
+//
+
+template <typename T>
+void NumCompException<T>::append(const std::string& extra) {
+  if (description != "") {
+    description += " ";
+  }
+  description += extra;
+}
+
+template <typename T>
+void NumCompException<T>::add_exc_data(const char* file, int line,
+                                       const char* function) {
+  std::stringstream ss;
+  ss << std::scientific << std::setprecision(15) << lhs << operation_string
+     << rhs << " (tol: " << tolerance << ")";
+  failed_condition = ss.str();
+  ExceptionBase::add_exc_data(file, line, function, failed_condition.c_str(),
+                              "NumCompException");
+}
+
+template <typename T>
+void NumCompException<T>::print_extra(std::ostream& out) const noexcept {
+  out << std::scientific << std::setprecision(15) << "Error in comparison ("
+      << error << ") larger than tolerance (" << tolerance << ").";
+  if (description != "") {
+    out << std::endl << description;
+  }
+}
+
+}  // namespace krims
