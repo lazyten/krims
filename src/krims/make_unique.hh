7629da19cc2288856037062b56ef8c8eae92fc94
Michael F. Herbst <info@michael-herbst.com>
2016-11-19 17:10:54 +0100

    std::make_unique-like function for convenience

---
+++ src/krims/make_unique.hh
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2016 by the krims authors
+//
+// This file is part of krims.
+//
+// krims is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// krims is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with krims. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+#include <memory>
+
+namespace krims {
+#ifdef KRIMS_HAVE_CXX14
+using std::make_unique;
+#else
+/** Function to mimic the c++14 function std::make_unique
+ *
+ * This implementation works, but has some loopholes, so we only use it
+ * until C++14 is wide-spread.
+ **/
+template <class T, class... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+#endif
+}  // namespace krims
