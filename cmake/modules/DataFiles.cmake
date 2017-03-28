dddd04a6e30ed6ad25274767d9709627adea0963
Michael F. Herbst <info@michael-herbst.com>
2017-03-28 15:30:59 +0200

    Clarify wording

--- cmake/modules/DataFiles.cmake
+++ cmake/modules/DataFiles.cmake
@@ -22,7 +22,7 @@
 # Module to manage the downloading and finding of files contaning
 # static data required to run a program.
 #
-# including this module will set the following variables:
+# including this module will set the following variables inside internal cache
 #   ${PROJECT_NAME}_DATA_DOWNLOAD_DIR
 #       The directory to which static data will be downloaded
 #   ${PROJECT_NAME}_DATA_INSTALL_DIR
@@ -44,7 +44,7 @@ set(${PROJECT_NAME}_DATA_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NA
 	CACHE INTERNAL "The directory where files for the project ${PROJECT_NAME} are installed")
 set(${PROJECT_NAME}_DATA_DOWNLOAD_DIR "${DATA_DOWNLOAD_DIR}/${PROJECT_NAME}"
 	CACHE INTERNAL "The directory where files for the project ${PROJECT_NAME}\
-are downloaded to during the build process")
+	are downloaded to during the build process")
 
 function(data_download_target TARGET URL HASHFULL)
 	# Add a target which downloads a static data tar.gz file
