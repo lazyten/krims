b06f0bd030669bd3b9303fa26267a99bf58eb86f
Michael F. Herbst <info@michael-herbst.com>
2017-06-23 13:14:21 +0200

    Disable the subscription pointer RCPWrapper for cheaply copyable

---
+++ src/krims/TypeUtils/IsSubscribable.hh
@@ -0,0 +1,34 @@
+//
+// Copyright (C) 2017 by the krims authors
+//
+// This file is part of krims.
+//
+// krims is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// krims is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with krims. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+#include "krims/Subscribable.hh"
+#include <type_traits>
+
+namespace krims {
+
+//@{
+/** \brief struct representing a type (std::true_type, std::false_type) which
+ *  indicates whether T is a object to which subscriptions can be made.
+ **/
+template <typename T>
+struct IsSubscribable : public std::is_base_of<Subscribable, T> {};
+//@}
+
+}  // namespace krims
